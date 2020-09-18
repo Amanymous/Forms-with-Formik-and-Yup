56d2b25589c76d4269689d4bcdac9a6e42a16d59
aman mirza <aman mirza99>
2020-09-18 22:44:29 +0500

    form with formik

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormikForm from './FormikForm';
+import './App.css';
+import FormikFormWithYup from './FormikFormWithYup';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FormikForm /> */}
+      <FormikFormWithYup />
+    </div>
+  );
+}
+
+export default App;
